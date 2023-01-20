ab60a32db9e69abdd91afc01c7f3ddfe8d4e7cd3
jzhan253 <jinfan.zhang@sjsu.edu>
2023-01-20 14:21:46 -0800

    button switched to svg

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Constants
+const editIcon = '<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>';
+
+const deletIcon = '<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>';
+
+const saveIcon = '<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21,20V8.414a1,1,0,0,0-.293-.707L16.293,3.293A1,1,0,0,0,15.586,3H4A1,1,0,0,0,3,4V20a1,1,0,0,0,1,1H20A1,1,0,0,0,21,20ZM9,8h4a1,1,0,0,1,0,2H9A1,1,0,0,1,9,8Zm7,11H8V15a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z"/></svg>';
+
+const cancelIcon = '<svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>'
+
+const addIcon = '<svg focusable viewBox="0 0 24 24" aria-hidden="true xmlns="http://www.w3.org/2000/svg"><path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+
 // API
 API = (() => {
   const URL = 'http://localhost:3000/events';
@@ -167,38 +178,21 @@ class EventsView {
     const eventActions = document.createElement('div');
     eventActions.classList.add('event-actions');
 
-    // const button1 = document.createElement('svg');
-    // button1.classList.add('event-button-1');
-    // if(adding) {
-    //   button1.xmlns = 'http://www.w3.org/2000/svg';
-    //   button1.focusable = true;
-    //   button1.viewBox = '0 0 24 24';
-    //   const buttonPath = document.createElement('path');
-    //   buttonPath.d = 'M12 6V18M18 12H6'
-    //   button1.value = 'Add';
-    //   button1.append(buttonPath);
-    // } else {
-    //   button1.textContent = 'Edit';
-    // }
-  
-    // const deleteButton = document.createElement('button');
-    // deleteButton.classList.add('delete-btn');
-    // deleteButton.textContent = 'Delete';
-
-    // eventActions.append(button1, deleteButton);
-    // tableCol4.append(eventActions);
-
     const button1 = document.createElement('button');
     button1.classList.add('event-button-1');
     if(adding) {
-      button1.textContent = 'Add';
+      button1.innerHTML = addIcon;
+      button1.setAttribute('btn_type', 'add');
     } else {
-      button1.textContent = 'Edit';
+      button1.innerHTML = editIcon;
+      button1.setAttribute('btn_type', 'save');
     }
   
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-btn');
-    deleteButton.textContent = 'Delete';
+    deleteButton.innerHTML = deletIcon;
+    deleteButton.setAttribute('btn_type', 'delete')
+    // deleteButton.textContent = 'Delete';
 
     eventActions.append(button1, deleteButton);
     tableCol4.append(eventActions);
@@ -209,20 +203,6 @@ class EventsView {
   }
 
   renderEvents(events) {
-  //   this.tableBody.innerHTML = `<tr class="table-header-row">
-  //   <td>
-  //     <h1>Event</h1>
-  //   </td>
-  //   <td>
-  //     <h1>Start</h1>
-  //   </td>
-  //   <td>
-  //     <h1>End</h1>
-  //   </td>
-  //   <td>
-  //     <h1>Actions</h1>
-  //   </td>
-  // </tr>`;
     const headerRow = document.createElement('tr');
     headerRow.classList.add('table-header-row');
     const td1 = document.createElement('td');
@@ -313,7 +293,7 @@ class EventsController {
           eventTitle.readOnly = false;
           eventStartDate.readOnly = false;
           eventEndDate.readOnly = false;
-          e.target.textContent = 'Save';
+          e.target.innerHTML = saveIcon;
           eventTitle.focus();
         } else {
           const titleValue = eventTitle.value;
@@ -322,7 +302,9 @@ class EventsController {
           if(!titleValue || titleValue === '' || !startDateValue || startDateValue === '' || !endDateValue || !endDateValue === '') {
             alert('please enter a valid value');
           } else {
-            if(eventBtn1.textContent === 'Add') {
+            console.log(eventBtn1);
+            console.log(eventBtn1.getAttribute('btn_type'));
+            if(eventBtn1.getAttribute('btn_type') === 'add') {
               this.model.addEvent({
                 eventName: titleValue,
                 startDate: startDateValue,
@@ -330,7 +312,7 @@ class EventsController {
               }).then(res => {
                 console.log(res);
               })
-            } else if(eventBtn1.textContent === 'Save') {
+            } else if(eventBtn1.getAttribute('btn_type') === 'save') {
               this.model.updateEvent(+domID, {
                 eventName: titleValue,
                 startDate: startDateValue,
@@ -342,7 +324,7 @@ class EventsController {
             eventTitle.readOnly = true;
             eventStartDate.readOnly = true;
             eventEndDate.readOnly = true;
-            e.target.textContent = 'Edit';
+            e.target.innerHTML = editIcon;
           }
         }
       } 
